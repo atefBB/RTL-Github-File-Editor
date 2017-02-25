642fcbc73e61c3bb79ff380b70a731b0608f4d56
atefBB <atef.bettaib@gmail.com>
2017-02-25 20:34:07 +0100

    clean-up

---
+++ background.js
@@ -0,0 +1,9 @@
+
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function() {
+  // No tabs or host permissions needed!
+  chrome.tabs.executeScript({
+    code: 'document.getElementsByClassName("CodeMirror-lines")[0]'
+                + '.setAttribute("style", "direction: rtl");'
+  });
+});
