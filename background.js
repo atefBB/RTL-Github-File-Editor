60c5656cc511abdfac67409b795b1c2248ef24ee
Atef Ben Ali <atef.bettaib@gmail.com>
2018-06-16 10:53:10 +0100

    make `readme.md` file review `rtl`

--- background.js
+++ background.js
@@ -5,7 +5,14 @@
 chrome.browserAction.onClicked.addListener(function() {
   // No tabs or host permissions needed!
   chrome.tabs.executeScript({
-    code: 'document.getElementsByClassName("CodeMirror-lines")[0]'
-          + '.setAttribute("style", "direction: rtl");'
+    code:
+      'if(document.getElementsByClassName("CodeMirror-lines")[0]) {' +
+      'document.getElementsByClassName("CodeMirror-lines")[0]' +
+      '.setAttribute("style", "direction: rtl");' +
+      "}" +
+      'if(document.getElementById("readme")) {' +
+      'document.getElementById("readme")' +
+      '.setAttribute("style", "direction: rtl");' +
+      "}"
   });
 });
